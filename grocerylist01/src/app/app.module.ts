768012c7a60e7f1c1b45465c4a76df9e89ee386a
Chris Gallup <chris.gallup@hotmail.com>
2016-11-25 23:29:10 -0600

    added content component

--- grocerylist01/src/app/app.module.ts
+++ grocerylist01/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
