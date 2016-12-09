8f010bf432351fd4000b4b229d5b995651e7b255
Chris Gallup <chris.gallup@hotmail.com>
2016-12-08 22:20:54 -0600

    added to content.component.html

--- grocerylist01/src/environments/firebase.config.ts
+++ grocerylist01/src/environments/firebase.config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
     // add you own api key here
-    apiKey: "",
+    apiKey: "yourkey",
     authDomain: "grocerylist-1528b.firebaseapp.com",
     databaseURL: "https://grocerylist-1528b.firebaseio.com",
     storageBucket: "grocerylist-1528b.appspot.com",
