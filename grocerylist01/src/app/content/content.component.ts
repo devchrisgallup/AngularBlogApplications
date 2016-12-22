ed3ff3877b7d55569a7a1b16131c3abad598e59d
Chris Gallup <chris.gallup@hotmail.com>
2016-12-22 17:23:06 -0600

    af object

--- grocerylist01/src/app/content/content.component.ts
+++ grocerylist01/src/app/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-content',
@@ -7,14 +8,16 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
-  items: FirebaseListObservable<any[]>;
+  items : FirebaseListObservable<any[]>;
   favoriteItem: FirebaseListObservable<any[]>;
   newItem: string = '';
-  newFavorite: string = ''; 
+  newFavorite: string = '';
   
 
-  constructor(private af: AngularFire) {}
+  constructor(private af: AngularFire) {
+    const items$ = af.database.object('/items');
+    items$.subscribe(console.log);
+  }
 
   ngOnInit() {
     this.items = this.af.database.list('/items');
@@ -22,9 +25,6 @@ export class ContentComponent implements OnInit {
   }
   
   add() {
-    // this.items.forEach(item => {
-    //     console.log('Item:', item);
-    // });
     this.items.push(this.newItem);
     this.newItem = '';
   }
