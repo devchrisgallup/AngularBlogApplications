1b848b3f36081c8160d06b52829cefc5544352b5
Chris Gallup <chris.gallup@hotmail.com>
2016-12-25 23:46:25 -0600

    button styles

--- grocerylist01/src/environments/firebase.config.ts
+++ grocerylist01/src/environments/firebase.config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
     // add you own api key here
-    apiKey: "YourKey",
+    apiKey: "https://www.gstatic.com/firebasejs/3.6.4/firebase.js",
     authDomain: "grocerylist-1528b.firebaseapp.com",
     databaseURL: "https://grocerylist-1528b.firebaseio.com",
     storageBucket: "grocerylist-1528b.appspot.com",
