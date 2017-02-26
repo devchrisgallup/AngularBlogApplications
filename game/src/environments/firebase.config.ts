3f27eba85d22c8013e722f836831a1177f1cf878
devchrisgallup <chris.gallup@hotmail.com>
2017-02-26 11:32:58 -0600

    Revert "Angular Toast Blog Post"
    This reverts commit f2d8a940d40202e7cc3e020eb7e0ffdbb8e2761e.

--- game/src/environments/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA8yXXtbWr5Qre9hJHadp3qoY1ACxlOx7M",
-    authDomain: "game-640a5.firebaseapp.com",
-    databaseURL: "https://game-640a5.firebaseio.com",
-    storageBucket: "game-640a5.appspot.com",
-}
