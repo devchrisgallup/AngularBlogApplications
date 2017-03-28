985bb0b9c5ac6200f58a4df54bc2a85b2d6d5626
devchrisgallup <chris.gallup@hotmail.com>
2017-03-27 21:41:40 -0500

    if else then conditional

--- ifelsethen/src/app/app.component.ts
+++ ifelsethen/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = true;
+
+  chageTheDay() {
+    if (this.title == true) {
+      this.title = false; 
+    } else if (this.title == false) {
+      this.title = true; 
+    }
+  }
 }
