32ff4d9328fcdaae5fb26c4eb4d8fa1e0677b4f2
devchrisgallup <chris.gallup@hotmail.com>
2017-05-21 17:17:40 -0500

    added animation

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -22,18 +22,33 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       // Direction, animate with druation and easing type
       transition('small <=> large', animate('300ms ease-in')),
     ]),
+    trigger('otherButton', [
+      state('small', style([{
+        transform: 'scale(1)',
+      }])),
+      state('large', style([{
+        transform: 'scale(1.5)',
+      }])),
+      transition('small <=> large', animate('500ms ease-in')),
+    ])
   ]
 })
 export class AppComponent {
   public title = 'Hover Over Me';
+  public otherTitle = 'Hover Some More'
   // change attribute value of animation
   public state: string = 'small'; 
+  public otherState: string = 'small'; 
   public clickCount: number = 0; 
 
   stateAnim() {
     // ternary operator to
     // toggle between states
     this.state = (this.state === 'small' ? 'large' : 'small');
+  }
+
+  otherAnim() {
+    this.otherState = (this.otherState === 'small' ? 'large' : 'small');
   }
 
   processData() {
