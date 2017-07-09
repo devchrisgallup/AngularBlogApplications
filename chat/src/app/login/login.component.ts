3b7764ddf8df500018d4cfb99ead7dac3bbc12b1
devchrisgallup <chris.gallup@hotmail.com>
2017-07-09 15:44:08 -0500

    firebase database

--- chat/src/app/login/login.component.ts
+++ chat/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
@@ -9,26 +10,39 @@ import * as firebase from 'firebase/app';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   providers: [
-    AngularFireAuth
+    AngularFireAuth,
+    AngularFireDatabase,
+    AngularFireModule
   ]
 })
 export class LoginComponent implements OnInit {
 
   public user: Observable<firebase.User>;
+  public items: FirebaseListObservable<any[]>;
 
-  constructor(public af: AngularFireAuth) {
+  constructor(public af: AngularFireAuth, public db: AngularFireDatabase) {
     this.user = af.authState;
    }
 
   ngOnInit() {
+    this.items = this.db.list('/item', {
+      query: {
+        limitToLast: 50
+      }
+    });
   }
 
   login() {
     this.af.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    console.log(this.items); 
   }
 
   logout() {
     this.af.auth.signOut();
+  }
+
+  data() {
+    this.items.push("Chris");
   }
 
 }
