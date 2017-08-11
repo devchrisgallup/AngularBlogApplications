94afb9f59eeaca55e81e950b2e802f8c3a173e8e
devchrisgallup <chris.gallup@hotmail.com>
2017-08-11 16:19:26 -0500

    pick games by date

--- angularmlb/src/app/game/game.component.ts
+++ angularmlb/src/app/game/game.component.ts
@@ -8,12 +8,20 @@ import { MlbService } from 'app/services/mlb.service';
 })
 export class GameComponent implements OnInit {
   public game; 
+  public dateplayed; 
   public homeName; 
   public awayName; 
+  public homecity;
+  public awaycity; 
   public score;
   public homescore = 0; 
   public awayscore = 0;  
-  public inning;  
+  public inning; 
+  private team = 'Royals'; 
+  private teams = ['Royals', 'Cardinals', 'Indians', 'Rockies', 'Mets', 'Rangers', 'Mariners', 'Athletics', 'Orioles', 'Angels', 'Cubs', 'Giants',
+                   'Marlins', 'Nationals', 'Yankees', 'Blue Jays', 'Rays', 'Red Sox', 'Pirates', 'Tigers', 'Padres', 'Reds', 'Phillies', 'Braves',
+                   'Astros', 'White Sox', 'Twins', 'Brewers', 'Dodgers', 'D-backs'];
+  
   constructor(private mlbService: MlbService) { }
 
   ngOnInit() {
@@ -25,35 +33,47 @@ export class GameComponent implements OnInit {
     this.logging(); 
   }
 
-  logging() {
-    console.log('called'); 
+  rundata() {
+    console.log(this.dateplayed); 
+    let datesplit = this.dateplayed.split("-"); 
+    let year = datesplit[0];
+    let month = datesplit[1]; 
+    let day = datesplit[2];
+    this.mlbService.getSelectedDate(year, month, day)
+                  .forEach(data => this.game = data.data.games.game);
+  }
+
+  logging() { 
     this.game.forEach(item =>  {
-      if (item.home_team_name == 'Royals') {
+      if (item.home_team_name == this.team) {
+        this.homecity = item.home_team_city; 
+        this.awaycity = item.away_team_city; 
         this.homeName = item.home_team_name;
         this.awayName = item.away_team_name; 
         this.inning = item.linescore.inning;
         this.score = item.linescore.inning;  
-      } else if (item.away_team_name == 'Royals') {
+      } else if (item.away_team_name == this.team) {
+        this.homecity = item.home_team_city; 
+        this.awaycity = item.away_team_city;
         this.homeName = item.home_team_name;
         this.awayName = item.away_team_name; 
         this.inning = item.linescore.inning;
         this.score = item.linescore.inning;
       } else {
-        console.log('Your team did not play today.');
+        // console.log('Your team did not play today.');
       }
     }); 
-    // Caculate 
+    // Caculate totals score for each  team
+    this.homescore = 0; 
+    this.awayscore = 0; 
     this.score.forEach(item => {
+      // if NaN 
       if (parseInt(item.home)) {
         this.homescore = this.homescore + parseInt(item.home); 
-      } else {
-        this.homescore = this.homescore; 
-      }
+      } 
       if (parseInt(item.away)) {
         this.awayscore = this.awayscore + parseInt(item.away); 
-      } else {
-        this.awayscore = this.awayscore; 
-      }
+      } 
     })
   }
 
--- angularmlb/src/app/services/mlb.service.ts
+++ angularmlb/src/app/services/mlb.service.ts
@@ -11,7 +11,15 @@ export class MlbService {
     // get data
     getData() {
         // http get request
-        this.dataUrl = 'http://mlb.mlb.com/gdcross/components/game/mlb/year_2017/month_08/day_09/master_scoreboard.json';
+        this.dataUrl = 'http://mlb.mlb.com/gdcross/components/game/mlb/year_2017/month_08/day_10/master_scoreboard.json';
+        return this.http.get(this.dataUrl)
+        .map(data => data.json());  
+    }
+
+    // get selected data
+    getSelectedDate(year, month, day) {
+        // http get request
+        this.dataUrl = 'http://mlb.mlb.com/gdcross/components/game/mlb/year_' + year + '/month_' + month + '/day_' + day + '/master_scoreboard.json';
         return this.http.get(this.dataUrl)
         .map(data => data.json());  
     }
